aab5151040e862c82167a8b63c197d1f6eb37440
PrestonPhelan <preston.phelan@gmail.com>
2017-04-19 12:40:03 -0700

    Client and Booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,14 @@
 class Booking < ApplicationRecord
+  belongs_to :client
+
+  validates :first_contact_date,
+    :source,
+    :status,
+    :type,
+    :price_seen,
+    presence: true
+
+  validates_inclusion_of :source, in: %w(DogVacay Rover Other)
+  validates_inclusion_of :status, in: %w(Pending Accepted Declined Cancelled)
+  validates_inclusion_of :type, in: %w(Boarding Daycare)
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,5 @@
 class Client < ApplicationRecord
+  validates :name, :num_dogs, :dogs, presence: true
+
+  has_many :bookings
 end
